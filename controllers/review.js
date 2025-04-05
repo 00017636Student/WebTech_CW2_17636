5a5ed6255e6780f7d05bc28e279dbfb8a7298281
00017636Student <baalcrush@gmail.com>
2025-04-05 20:14:17 +0500

    created controllers

---
+++ controllers/review.js
@@ -0,0 +1,69 @@
+const { validationResult } = require('express-validator');
+const reviewService = require('../services/review');
+const reviewControl = {
+
+getById: (req, res) => {
+try {
+const review = reviewService.getById(req, res);
+res.render('review/CreateAndUpdate', { review })
+} catch (error) {
+res.status(500).json({ error: error.message });
+}},
+
+
+getAll: (req, res) => {
+try {
+const reviews = reviewService.get(req, res);
+res.render('review/index', { reviews })
+} catch (error) {
+res.status(500).json({ error: error.message });
+}},
+
+
+create: async (req, res) => {
+const errors = validationResult(req);
+if (!errors.isEmpty()) {
+return res.render('review/CreateAndUpdate', { errors: errors.array(), review: req.body });
+}
+try {
+await reviewService.insert(req, res);
+res.redirect('/review');
+} 
+catch (error) {
+res.status(500).json({ error: error.message });
+}},
+
+
+update: async (req, res) => {
+const errors = validationResult(req);
+if (!errors.isEmpty()) {
+return res.render('review/CreateAndUpdate', { errors: errors.array(), review: req.body });
+}
+try {
+const updatedreview = reviewService.update(req, res);
+if (!updatedreview) {
+return res.status(404).json({ error: 'Review' });
+}
+res.redirect('/review');
+} catch (error) {
+res.status(500).json({ error: error.message });
+}},
+
+
+delete: (req, res) => {
+try {
+const removedReview = reviewService.delete(req, res);
+
+if (!removedReview) {
+return res.status(404).json({ error: 'review not found' });
+}
+res.redirect('/review');
+} 
+catch (error) {
+res.status(500).json({ error: error.message });
+}
+}
+};
+
+
+module.exports = reviewControl
