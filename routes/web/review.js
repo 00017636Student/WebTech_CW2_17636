8d2f0bfb360423917d4d741433eb61fad288fdbc
00017636Student <baalcrush@gmail.com>
2025-04-07 01:11:16 +0500

    commenting services

--- routes/web/review.js
+++ routes/web/review.js
@@ -3,18 +3,15 @@ const reviewControl = require('../../controllers/review');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-    reviewControl.getAll(req, res)
-    });
-
+reviewControl.getAll(req, res)
+});
 
 router.get('/create', (req, res)=>{
 res.render('review/CreateAndUpdate')
-
 });
 
 router.get('/update/:id', (req, res)=>{
-    reviewControl.getById(req, res)
-    });
-
+reviewControl.getById(req, res)
+});
 
 module.exports = router;
