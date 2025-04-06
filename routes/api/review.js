e8c837c75828e858933f5a541b7d156fbe0a0581
00017636Student <baalcrush@gmail.com>
2025-04-06 13:11:28 +0500

    removed spaces

--- routes/api/review.js
+++ routes/api/review.js
@@ -1,9 +1,7 @@
-
 const express = require('express');
 const router = express.Router();
 const { reviewValidation } = require('../../validators/review');
 const reviewControl = require('../../controllers/review');
-
 
 router.post('/create', reviewValidation(), reviewControl.create);
 router.post('/update/:id', reviewValidation(), reviewControl.update);
